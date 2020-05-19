cdd3840fab4628e003132b8f1944b72a7443b7ae
SafeyahShemali <safonorthly@gmail.com>
2020-05-19 20:23:24 +0300

    Initial Commit

---
+++ Vincent_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vincent_App
+//
+//  Created by Safeyah on 5/19/20.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
